52f505d247743b24927fb9da55b3c1232ee42484
Mahnoor <mahnoort104@gmail.com>
2025-02-06 01:52:56 +0500

    Add files via upload

---
+++ src/app/cancel/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+export default function Cancel() {
+    return (
+      <div className="text-center mt-10">
+        <h1 className="text-2xl font-bold">Payment Cancelled ❌</h1>
+        <p>You can try again anytime.</p>
+      </div>
+    );
+  }
+  
